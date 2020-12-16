04a43ef49abd587f4061b9450e439d10e5590940
MatiasForconi <matiasfernandoforconi@hotmail.com>
2020-12-16 20:58:10 -0300

    added middleware

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,9 +2,9 @@ import { Router } from 'express';
 import bcrypt from 'bcryptjs';
 import config from '../config';
 import jwt from 'jsonwebtoken';
-import auth from '../../middleware/auth';
+import auth from '../middleware/auth';
 // User Model
-import User from '../../models/User';
+import User from '../models/User';
 
 const { JWT_SECRET } = config;
 const router = Router();
